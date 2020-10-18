d51eb22ee9a79dd1a766b113bd99f69c80429843
myucesan <my96@live.nl>
2020-10-18 13:08:36 +0200

    added hardcoded rolecall

--- RoleCall.tsx
+++ RoleCall.tsx
@@ -2,19 +2,63 @@ import React, { Component } from "react";
 
 import "tabler-react/dist/Tabler.css";
 
-import { Card, Button } from "tabler-react";
+import { ContactCard, Card, Button, Grid } from "tabler-react";
 
 export class RoleCall extends Component {
   render() {
     return (
-      <Card>
-        <Card.Header>
-          <Card.Title>Card Title</Card.Title>
-        </Card.Header>
-        <Card.Body>
-          <Button color="primary">A Button</Button>
-        </Card.Body>
-      </Card>
+      <div>
+        <h1>Role Call </h1>
+        <Grid.Row cards deck>
+          <Grid.Col md={4}>
+            <ContactCard
+              rounded
+              name={"Death Lurks"}
+              details={[
+                { title: "Position", content: "Leadership" },
+                { title: "Profession", content: "PvP | Ratting" },
+                { title: "Manufacturing skills", content: "None" },
+                { title: "Timezone", content: "EU" },
+                { title: "Accounts", content: "1" },
+                { title: "Characters", content: "1" }
+
+              ]}
+              description={`
+Character Name(s): Death Lurks`}
+            />
+          </Grid.Col>
+          <Grid.Col md={4}>
+            <ContactCard
+              rounded
+              name={"Zell"}
+              details={[
+                { title: "Position", content: "Member" },
+                { title: "Profession", content: "Ratting | Miner" },
+                { title: "Manufacturing skills", content: "Rare Ore [5/4], Strip Mining [5/5/3]" },
+                { title: "Timezone", content: "AU (UTC +8)" },
+                { title: "Accounts", content: "2" },
+                { title: "Characters", content: "2" }
+              ]}
+              description={`Character Name(s): Zell / Inskaral Pus`}
+            />
+          </Grid.Col>
+          <Grid.Col md={4}>
+            <ContactCard
+              rounded
+              name={"WhiteWolf"}
+              details={[
+                { title: "Position", content: "Leadership" },
+                { title: "Profession", content: "Miner | Ratting" },
+                { title: "Manufacturing skills", content: "Frigate [5/5/4], Cruiser [5/5/4]" },
+                { title: "Timezone", content: "BST (UTC + 6)" },
+                { title: "Accounts", content: "3" },
+                { title: "Characters", content: "3" }
+              ]}
+              description={`Character Name(s): WhiteWoIf, Artemis', Divine Miner`}
+            />
+          </Grid.Col>
+        </Grid.Row>
+      </div>
     );
   }
 }
--- index.tsx
+++ index.tsx
@@ -21,8 +21,6 @@ class App extends Component<AppProps, AppState> {
     return (
     
       <div>
-        <Hello name={this.state.name} />
-        <p>Start editing to see some magic happen :)</p>
         <RoleCall />
       </div>
     );
